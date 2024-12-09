55d61ec78d01133cabdbc9c7c47e3ef2f2749576
Konrad Falkowski <konradfalkow13@gmail.com>
2024-12-09 09:21:11 +0100

    Nowe Zmiany

---
+++ filters.py
@@ -0,0 +1,172 @@
+import numpy as np
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+from inne import saveImg
+
+
+# Przykładowy filtr działanie:
+# Filtruje zaszumiony obraz z scieżki: "outputFolder/NoisyImg.jpg"
+# potem zapisuje ten obraz do outputFolder jako FilImg.png
+# robi to żeby później można było go zapisać do folderu "ZapisaneObrazy" po kliknięciu Zapisz obraz
+    
+def adaptive_median_filter(img_path, label, max_window_size = 7):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    h, w = img.shape
+    filtered_img = np.copy(img)
+
+    def get_median(window):
+        return np.median(window)
+
+    for i in range(h):
+        for j in range(w):
+            window_size = 3  # Początkowy rozmiar okna
+            while window_size <= max_window_size:
+                half_size = window_size // 2
+
+                # Ustalanie granic okna
+                x1 = max(0, i - half_size)
+                x2 = min(h, i + half_size + 1)
+                y1 = max(0, j - half_size)
+                y2 = min(w, j + half_size + 1)
+
+                window = img[x1:x2, y1:y2]
+                z_min, z_max, z_med = np.min(window), np.max(window), get_median(window)
+
+                if z_min < z_med < z_max:
+                    # Jeśli piksel nie jest szumem, pozostawiamy go
+                    if z_min < img[i, j] < z_max:
+                        filtered_img[i, j] = img[i, j]
+                    else:
+                        filtered_img[i, j] = z_med
+                    break
+                else:
+                    window_size += 2  # Zwiększamy rozmiar okna
+
+                # Jeśli osiągnięto maksymalny rozmiar okna, ustawiamy medianę
+                if window_size > max_window_size:
+                    filtered_img[i, j] = z_med
+
+    # Przeskalowanie obrazu do wyświetlenia
+    
+    displayImg = Image.fromarray(filtered_img)
+    displayImg = displayImg.resize((300, 300), Image.LANCZOS)
+    
+    # Wyświetlenie obrazu w Tkinterze
+    
+    img = ImageTk.PhotoImage(displayImg)
+    label.config(image=img)
+    label.image = img
+    
+    # Zapis obrazu do folderu
+    
+    saveImg(filtered_img, 'outputFolder', 'FilImg.png')
+    
+def adaptive_wiener_filter(img_path, label):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    filtered_img = cv2.medianBlur(img, 5)
+    
+    # Przeskalowanie obrazu do wyświetlenia
+    
+    displayImg = Image.fromarray(filtered_img)
+    displayImg = displayImg.resize((300, 300), Image.LANCZOS)
+  
+    # Wyświetlenie obrazu w Tkinterze
+    
+    img = ImageTk.PhotoImage(displayImg)
+    label.config(image=img)
+    label.image = img
+    
+    # Zapis obrazu do folderu
+    
+    saveImg(filtered_img, 'outputFolder', 'FilImg.png')
+
+def adaptive_gaussian_filter(img_path, label, kernel_size=15, max_sigma=5.0):
+    """
+    Implementacja adaptacyjnego filtra Gaussa.
+    """
+    # Wczytanie obrazu
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    
+    if img is None:
+        print("Nie udało się wczytać obrazu.")
+        return
+
+    # Przekształcenie do float32 dla większej precyzji
+    img = img.astype(np.float32)
+
+    # Obliczenie lokalnej wariancji
+    mean = cv2.blur(img, (kernel_size, kernel_size))
+    squared_mean = cv2.blur(img**2, (kernel_size, kernel_size))
+    variance = squared_mean - mean**2
+
+    # Mapowanie wariancji do wartości sigma
+    variance = np.clip(variance, 0, np.max(variance))
+    sigma_map = max_sigma * variance / np.max(variance)
+
+    # Przygotowanie obrazu wynikowego
+    output = np.zeros_like(img, dtype=np.float32)
+
+    # Padding obrazu, aby uniknąć problemów z brzegami
+    padded_img = np.pad(img, ((kernel_size // 2,), (kernel_size // 2,)), mode='reflect')
+
+    # Przetwarzanie każdego piksela
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            sigma = sigma_map[i, j]
+            size = kernel_size // 2
+
+            # Wycinanie lokalnego obszaru
+            y_min, y_max = i, i + kernel_size
+            x_min, x_max = j, j + kernel_size
+            patch = padded_img[y_min:y_max, x_min:x_max]
+
+            # Tworzenie lokalnego jądra Gaussa
+            y_indices, x_indices = np.ogrid[-size:size+1, -size:size+1]
+            kernel = np.exp(-(x_indices**2 + y_indices**2) / (2 * sigma**2))
+            kernel /= kernel.sum()
+
+            # Zastosowanie jądra
+            output[i, j] = np.sum(patch * kernel)
+
+    # Normalizacja wyniku do zakresu 0-255
+    output = np.clip(output, 0, 255).astype(np.uint8)
+
+    # Przeskalowanie obrazu do wyświetlenia
+    displayImg = Image.fromarray(output)
+    displayImg = displayImg.resize((300, 300), Image.LANCZOS)
+
+    # Wyświetlenie obrazu w Tkinterze
+    img_display = ImageTk.PhotoImage(displayImg)
+    label.config(image=img_display)
+    label.image = img_display
+
+    # Zapis obrazu do folderu
+    saveImg(output, 'outputFolder', 'FilImg.png')
+
+
+
+def bilateral_filter(img_path, label, diameter=15, sigma_color=75, sigma_space=75):
+    """
+    Implementacja filtra bilateralnego z użyciem OpenCV.
+    """
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    
+
+    # Filtracja z użyciem OpenCV
+    filtered_img = cv2.bilateralFilter(img, diameter, sigma_color, sigma_space)
+
+    # Przeskalowanie obrazu do wyświetlenia
+    displayImg = Image.fromarray(filtered_img)
+    displayImg = displayImg.resize((300, 300), Image.LANCZOS)
+
+    # Wyświetlenie obrazu w Tkinterze
+    img_display = ImageTk.PhotoImage(displayImg)
+    label.config(image=img_display)
+    label.image = img_display
+
+    # Zapis obrazu do folderu
+    saveImg(filtered_img, 'outputFolder', 'FilImg_Bilateral_OpenCV.png')
+
+
+    
